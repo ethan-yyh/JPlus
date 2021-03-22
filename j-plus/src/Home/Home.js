6bb5c697fe6eccd0da9e891aaa5f97f781dc02f2
ethanyao <yaox0570@mylaurier.ca>
2021-03-22 10:21:39 -0400

    adding dependencies

---
+++ j-plus/src/Home/Home.js
@@ -0,0 +1,13 @@
+// import files
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component{
+    render(){
+        return(
+            <p>Home Page</p>
+        );
+    } // end of render
+} // end of Home
+
+export default Home
