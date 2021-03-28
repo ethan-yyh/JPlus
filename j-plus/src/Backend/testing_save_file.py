9308896569fdfd1cff4440af6c98aa4670bb4bbc
zyWangWayne <37949666+zyWangWayne@users.noreply.github.com>
2021-03-28 16:35:05 -0400

    upload file  encounter 500 error

--- j-plus/src/Backend/testing_save_file.py
+++ j-plus/src/Backend/testing_save_file.py
@@ -9,5 +9,27 @@ cgitb.enable()
 # # Create instance of FieldStorage
 form = cgi.FieldStorage()
 
+# Get filename here.
+fileitem = form['filename']
+print(fileitem)
+# Test if the file was uploaded
+if fileitem.filename:
+   # strip leading path from file name to avoid
+   # directory traversal attacks
+   fn = os.path.basename(fileitem.filename)
+   open('/tmp/' + fn, 'wb').write(fileitem.file.read())
+   message = 'The file "' + fn + '" was uploaded successfully'
+ 
+else:
+   message = 'No file was uploaded'
+ 
+print ("""\
+Content-Type: text/html\n
+<html>
+<body>
+   <p>%s</p>
+</body>
+</html>
+""" % (message,))
 # implement the rest to get file and its name
 # make sure to include extension ex. resume.pdf
