463e50dd985a27fbfcffa7cb99270a874f4a90d1
ethanyao <yaox0570@mylaurier.ca>
2021-04-06 19:37:18 -0400

    updated function on resume upload

--- j-plus/src/components/UploadResume.js
+++ j-plus/src/components/UploadResume.js
@@ -11,7 +11,8 @@ class UploadResume extends Component{
             disabled: "disabled"
         }
 
-        this.updateFileName = this.updateFileName.bind(this)
+        this.updateFileName = this.updateFileName.bind(this);
+        this.uploadFile = this.uploadFile.bind(this);
     }
 
     componentDidMount(){
@@ -23,6 +24,8 @@ class UploadResume extends Component{
         event.preventDefault();
 
         var fullPath = document.getElementById("select-file-btn").value;
+
+        console.log(fullPath)
 
         if (fullPath){
             var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
@@ -39,17 +42,46 @@ class UploadResume extends Component{
         }
     }
 
+    uploadFile(event){
+
+        event.preventDefault();
+
+        var input = document.querySelector('input[type="file"]')
+
+        console.log("upload file called.. ")
+        console.log("files: " + input.files[0])
+
+        fetch('http://localhost:9000/uploadAPI', {
+            method: 'POST',
+            body: input.files[0]
+        })
+        .then(response => {
+            console.log(response);
+            document.getElementById('file-uploaded-successfully').innerHTML = 
+            `
+            <div class="alert alert-success">
+            <strong>Uploaded Successfully</strong> Your file was uploaded successfully. Refresh the page to see the keywords we captured based on your resume.
+            </div>
+            `
+        });
+
+
+    }
+
+
+
 
     render(){
         return(
             <div>
-                <form>
+                <form onSubmit={this.uploadFile}>
                     <div className="form-group">
                         <label className="btn btn-outline-primary" htmlFor="select-file-btn" id="select-file-btn-label" >Select File</label>
                         <input type="file" className="form-control-file" id="select-file-btn" onChange={this.updateFileName}></input>
                         <button className="btn btn-outline-primary" type="submit" id="upload-btn">Upload</button>
                         <small className="form-text text-muted" id="file-name">You selected: {this.state.filename}</small>
-                        
+                        <br></br>
+                        <div id="file-uploaded-successfully"></div>
                     </div>
                 </form>
             </div>
